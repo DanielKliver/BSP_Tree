073a76a013b08d124fb7e8f24bdd40a355e76223
DanielKliver <povishevd@mail.ru>
2024-01-09 04:12:07 +0700

    added a few more features

--- src/main.cpp
+++ src/main.cpp
@@ -73,15 +73,47 @@ class BSP_CMP
 {
 	private:
 
+	int *vector_product(int *edge1, int *edge2)
+	{
+		//векторное произведение
+	}
+
+	int *find_normal(polygon list_of_polygons)
+	{
+		//скорее всего нуждается в рефакторинге
+		int *edge1 = new int[3];
+		int *edge2 = new int[3];
+		//вычисление двух векторов полигона для дальнейшего поиска нормали 
+		//и составления уравнения плоскости
+		edge1[0] = list_of_polygons.two.x - list_of_polygons.one.x;
+		edge1[1] = list_of_polygons.two.y - list_of_polygons.one.y;
+		edge1[2] = list_of_polygons.two.z - list_of_polygons.one.z;
+
+		edge1[0] = list_of_polygons.three.x - list_of_polygons.one.x;
+		edge1[1] = list_of_polygons.three.y - list_of_polygons.one.y;
+		edge1[2] = list_of_polygons.three.z - list_of_polygons.one.z;
+		//////////////////////////////////////////////////////////////////
+		
+		vector_product(edge1, edge2);
+
+
+		//надо удалить весь массив
+		delete edge1;
+		delete edge2;
+	}
+
 	vector<polygon> classify_polygon(vector<polygon> list_of_polygons, vector<polygon> front_list, vector<polygon> back_list)
 	{
 		//вычислить два ребра и выполнить операцию векторного произведения
+		int *normal = new int[3];
+		normal = find_normal(list_of_polygons[0]);
 		//составить уравнение плоскости вида 0x(x-1)-7x(y-12)+0x(z-0)
 		//подставлять в него значения первых двух точек каждого полигона, добавлять в back/front
 		//удалить первый полигон из вектора, вернуть его
 	}
 
 	public:
+
 	BSP_CMP(node* root, vector<polygon> list_of_polygons)
 	{
 		if(root->id_of_polygon == 0)
